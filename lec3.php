52c25908af87f7e9f415b483d25b9c76a2f1e5a9
doha9 <allahamduha2@gmail.com>
2022-08-02 10:18:20 +0300

    third commit

--- lec3.php
+++ lec3.php
@@ -7,10 +7,26 @@ for ($x = 0; $x <= 100; $x+=10) {
 
 
   echo"<br>";echo"<br>";echo"<br>";
- $colors = array("red", "green", "blue", "yellow");
+ $colors = array("red", "green", "blue");
 
 for($x = 0; $x <= count($colors); $x++) {
   echo  "$colors[$x]<br>";
 }
+//loops******foreach******
+
+$colors = array("red", "green", "blue");
+
+foreach ($colors as $value) {
+  echo "$value <br>";
+}
+
+echo"<br>";echo"<br>";echo"<br>";
+
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $val) {
+  echo "The number is:  $x = $val<br>";
+}
+
 
 ?>
---
+++ lec4.php
@@ -0,0 +1,22 @@
+<?php
+
+//loops******while******
+$x = 1;
+
+while($x <= 5) {
+  echo "The number is:  $x <br>";
+  $x++;
+}
+echo"<br>";echo"<br>";echo"<br>";
+//loops******  do   while******
+$x = 1;
+
+do {
+  echo "The number is:  $x <br>";
+  $x++;
+} while ($x <= 5);
+
+
+
+
+?>
