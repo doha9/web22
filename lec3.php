64d94c970480c20eb252cc123067126c76255fd9
doha9 <allahamduha2@gmail.com>
2022-08-02 09:54:19 +0300

    third commit

---
+++ lec3.php
@@ -0,0 +1,16 @@
+<?php
+//loops******for******
+
+for ($x = 0; $x <= 100; $x+=10) {
+    echo "The number is: $x <br>";
+  }
+
+
+  echo"<br>";echo"<br>";echo"<br>";
+ $colors = array("red", "green", "blue", "yellow");
+
+for($x = 0; $x <= count($colors); $x++) {
+  echo  "$colors[$x]<br>";
+}
+
+?>
