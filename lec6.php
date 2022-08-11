5d05928793cd883ffdb640f6af0ec85d73c9225d
doha9 <allahamduha2@gmail.com>
2022-08-11 10:11:58 +0300

    sixthly commit

--- lec6.php
+++ lec6.php
@@ -21,12 +21,25 @@ function check_palindrome($string)
   else
   echo "notpalindrome";
 }
-echo check_palindrome('pop')."\n";
+echo check_palindrome('pop')."<br>";
 echo check_palindrome('madam')."<br>";
 echo check_palindrome('meme')."<br>";
 
 echo"<br>";
 
 
-
+function reverse_string($str1)
+{
+ $n = strlen($str1);
+ if($n == 1)
+   {
+    return $str1;
+   }
+ else
+   {
+   $n--;
+   return reverse_string(substr($str1,1, $n)) . substr($str1, 0, 1);
+   }
+}
+print_r(reverse_string('1234')."\n");
 ?>
