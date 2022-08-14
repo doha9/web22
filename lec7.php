be58f2660089ec727ba9a11989c63aad108d6d95
doha9 <allahamduha2@gmail.com>
2022-08-14 12:07:20 +0300

    seventh commit

--- lec7.php
+++ lec7.php
@@ -1,3 +1,10 @@
 <?php
-echo"dodo";
+$d = strtotime("Sunday");
+$f = strtotime("+6 weeks", $d);
+
+while ($d < $f) {
+    echo date("M d", $d) . "<br>";
+    $d = strtotime("+1 week", $d);
+  }
+ 
 ?>
