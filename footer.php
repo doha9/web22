9bd206abf1166282d93c0d77cc40a537c2074ab0
doha9 <allahamduha2@gmail.com>
2022-08-21 11:47:17 +0300

    eighth commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+<?php
+echo"hi dodo";
+echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
+?>
---
+++ lec8.php
@@ -0,0 +1,15 @@
+
+
+<html>
+<body>
+
+
+<?php include 'footer.php';?>
+
+
+<h1>Welcome to my home page dodo!</h1>
+
+
+</body>
+</html>
+
